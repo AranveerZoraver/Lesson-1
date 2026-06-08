f1fe919abb8e1aaca5d0d0f507c824b9f6e1b806
AranveerZoraver <aranveer.singh777@gmail.com>
2026-06-08 15:43:43 +0400

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,9 @@
+print ("This is my daily routine\n")
+
+print ("I wake up at 6:30 AM")
+
+print ("then i go to school at 7:30 AM")
+
+print ("After school i take a shower and then play with my friends")
+
+print ("and then i study for an hour")
