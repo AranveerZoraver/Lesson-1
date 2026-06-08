7b7a37809989222caf685b6c76747a9556a981f8
AranveerZoraver <aranveer.singh777@gmail.com>
2026-06-08 15:25:42 +0400

    Add files via upload

---
+++ Main1.py
@@ -0,0 +1,11 @@
+#Print a sentence
+print ("hello world")
+
+#Print a number
+print (4)
+
+#Next Line after print
+print ("Hello python \n")
+
+#Pass multiple arguments for printing
+print ("welcome to " , end= "+")
