a3ecbbd3d6fcc02de7f47ea283e6eba4046ba929
AranveerZoraver <aranveer.singh777@gmail.com>
2026-06-15 14:53:16 +0400

    Add files via upload

---
+++ Datatype.py
@@ -0,0 +1,11 @@
+a = 5
+print ("type of a: ", type(a))
+
+b = 1.5
+print ("type of b: ", type(b))
+
+c = "Hello my name is Aranveer"
+print ("type of c: ", type(c))
+
+d = False
+print ("type of d: ", type(d))
