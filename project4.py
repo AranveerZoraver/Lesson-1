a5f98dc9ae33cf9deafd8c334324515a64218119
AranveerZoraver <aranveer.singh777@gmail.com>
2026-07-01 14:18:58 +0400

    Add files via upload

---
+++ project4.py
@@ -0,0 +1,8 @@
+temperature = float(input("What's the temperature today (°C)? "))
+
+if temperature >= 25:
+    print("It's warm Rohan can wear something light and soft.")
+elif temperature >= 15:
+    print("It's mild — a light jacket should do.")
+else:
+    print("It's cold Rohan should wear his jacket and pullover.")
