ef8d5815b12628c35a5b5db8fcd5cece87cfac06
AranveerZoraver <aranveer.singh777@gmail.com>
2026-07-06 14:59:56 +0400

    Add files via upload

---
+++ andor.py
@@ -0,0 +1,24 @@
+a = 30
+b = 25
+c = 0
+
+if a and b and c:
+    print("all numbers in boolean are true")
+else:
+    print("atlease one of the numbers in boolean are true")
+
+
+z = 25
+x = -10
+c = 0
+
+if z > 0  or x > 0:
+    print("either of them are true")
+else:
+    print("one of them are true")
+
+
+if x > 0 or c > 0:
+    print("either of them are true")
+else:
+    print("both are false")
