a76e91ff60400a042e9de468043d672e4bb8c3fc
AranveerZoraver <aranveer.singh777@gmail.com>
2026-07-08 15:06:34 +0400

    Add files via upload

---
+++ identidyopertor.py
@@ -0,0 +1,33 @@
+x = 5
+
+if (type(x) is int):
+
+    print("true")
+
+else:
+
+    print("false")
+
+
+x = 5.5
+
+if (type(x) is float):
+
+    print("true")
+
+else:
+
+    print("false")
+
+
+c = 35
+
+v = 35
+
+if (c is v):
+    print("both are same")
+
+y = 20
+
+if (c is not y):
+    print("both are not the same")
