a43e166e2e09939c07f3c1c712956791d872f216
AranveerZoraver <aranveer.singh777@gmail.com>
2026-07-22 14:59:44 +0400

    Add files via upload

---
+++ operatorprecedence.py
@@ -0,0 +1,13 @@
+w = 3
+y = 5
+x = 4
+z = 0
+z = (w+y)*x/y
+print (z)
+
+name = "Jack"
+age = 0
+if name == "Jack" or name == "adam" and age >=3 :
+    print("Hello welcome")
+else:
+    print("Good bye!")
